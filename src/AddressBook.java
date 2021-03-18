68c63e0b88c32fc41095d7adb6c02ccfc40b4722
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-18 07:53:05 +0530

    [sai] created a weklcome message

---
+++ src/AddressBook.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+    public static void main (String [] args) {
+        System.out.println("-----Welcome to Address Book Program-----");
+        //AddressBook addressbook = new AddressBook();
+    }
+
+}
