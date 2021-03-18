c7a71eda8b7ec7b2b43f941f8abd704aa8081dd2
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-18 08:29:40 +0530

    [sai] Added multiple contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -27,10 +27,46 @@ public class AddressBook {
         System.out.println("Contact added Successfully");
     }
 
+    public void deleteContact() {
+        Scanner sc = new Scanner(System.in);
+        if (contacts.isEmpty()) {
+            System.out.println("Contact list is empty.");
+        } else {
+            System.out.println("Enter the first name to delete contact.");
+            String name = sc.next();
+            Iterator<Integer> itr = contacts.keySet().iterator();
+            while(itr.hasNext()) {
+                int key = itr.next();
+                if (contacts.get(key).firstName.equals(name)) {
+                    contacts.remove(key);
+                    System.out.println("Contact deleted with first name : "+name);
+                }
+            }
+        }
+    }
+
     public static void main (String [] args) {
         System.out.println("-----Welcome to Address Book Program-----");
+        Scanner sc = new Scanner(System.in);
+        int choice = 1;
         AddressBook addressbook = new AddressBook();
-        addressbook.addContact();
+        do {
+            System.out.println("Enter your choice\n1. Add contact\n2. Delete Contact\n3. Exit");
+            int userInput = sc.nextInt();
+            switch (userInput){
+                case 1 :
+                    addressbook.addContact();
+                    break;
+                case 2 :
+                    addressbook.deleteContact();
+                    break;
+                default:
+                    System.out.println("You press exit.\nThank You!");
+                    choice = 0;
+                    break;
+            }
+        }
+        while(choice != 0);
     }
 
 }
